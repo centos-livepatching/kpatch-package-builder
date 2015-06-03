b678871b4d14b91f40798e7489a08126c0c2dc21
Louis Taylor <louis@kragniz.eu>
2015-06-03 21:37:57 +0100

    Add argument parsing

--- build.py
+++ build.py
@@ -1,5 +1,7 @@
+import argparse
 import os
 import string
+
 
 def generate_rpm_spec(template, patch_file):
     spec_template = string.Template(template)
@@ -19,8 +21,15 @@ def generate_rpm_spec(template, patch_file):
     return spec_template.substitute(values)
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Generate RPM spec file to '
+                                                 'build a kpatch package')
+    parser.add_argument('patch', metavar='PATCH',
+                        help='patch file from which to build the livepatch '
+                             'module')
+
+    args = parser.parse_args()
 
     with open('kpatch-patch.spec') as f:
         template = f.read()
 
-    print(generate_rpm_spec(template, 'example.patch'))
+    print(generate_rpm_spec(template, args.patch))
