d8ecfc29e3fa7db1a3adb4122ac4867434df470f
Louis Taylor <louis@kragniz.eu>
2015-06-03 22:09:38 +0100

    Add -a --arch option

--- build.py
+++ build.py
@@ -34,7 +34,7 @@ cp -f "$kmod_filename" "%{buildroot}/%{installdir}/%{kernel}"
 %{installdir}/%{kernel}/$kmod_filename
 '''
 
-def generate_rpm_spec(template, patch_file, kernel):
+def generate_rpm_spec(template, patch_file, kernel, arch):
     spec_template = string.Template(template)
 
     base_name, _ = os.path.splitext(patch_file)
@@ -46,7 +46,7 @@ def generate_rpm_spec(template, patch_file, kernel):
         'description': 'Package generated from {} by '
                        'kpatch-package-builder'.format(patch_file),
         'target_kernel': kernel,
-        'target_arch': 'x86_64',
+        'target_arch': arch,
     }
 
     return spec_template.substitute(values)
@@ -63,13 +63,17 @@ def get_args():
     parser.add_argument('-k', '--kernel', metavar='version', default='3.10.0-229.el7',
                         help='target kernel version to build the livepatch '
                              'module against')
+    parser.add_argument('-a', '--arch', metavar='arch', default='x86_64',
+                        help='architecture to compile the patch against')
 
     return parser.parse_args()
 
 if __name__ == '__main__':
     args = get_args()
 
-    spec_content = generate_rpm_spec(SPEC_TEMPLATE, args.patch, args.kernel)
+    spec_content = generate_rpm_spec(SPEC_TEMPLATE, args.patch,
+                                                    args.kernel,
+                                                    args.arch)
 
     with open(args.output, 'w') as f:
         f.write(spec_content)
